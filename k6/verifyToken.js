bc2630fdfc6789cb219b7c25ac88fb7d1a8f5b08
betxcircle <150256421+betxcircle@users.noreply.github.com>
2025-05-20 00:25:09 +0100

    Create verifyToken.js

---
+++ k6/verifyToken.js
@@ -0,0 +1,21 @@
+// middleware/verifyToken.js
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = process.env.JWT_SECRET;
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers['authorization'];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Access denied. No token provided.' });
+  }
+
+  try {
+    const decoded = jwt.verify(token.replace('Bearer ', ''), SECRET_KEY);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(403).json({ message: 'Invalid or expired token.' });
+  }
+};
+
+module.exports = verifyToken;

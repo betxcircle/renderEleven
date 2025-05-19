4f4a2b2d7f4b494718a4d9327fb84961e1df8c17
betxcircle <150256421+betxcircle@users.noreply.github.com>
2025-05-19 14:39:03 +0100

    Update FaceOffAnswerModel.js

--- models/FaceOffAnswerModel.js
+++ models/FaceOffAnswerModel.js
@@ -1,27 +1,44 @@
 const mongoose = require('mongoose');
 
 const faceOffAnswerSchema = new mongoose.Schema({
-  batchId: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'Batch', 
-    required: true 
-  },
-  userAnswers: [
-    {
-      userId: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'User', 
-        required: true 
-      },
-      correctAnswerCount: { 
-        type: Number, 
-        required: true
+  {
+    batchId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'Batch',
+    },
+    batchName: {
+      type: String,
+      required: true,
+    },
+    totalBetAmount: {
+      type: String,
+      required: true,
+    },
+    userAnswers: [
+      {
+        userId: { type: String, required: true },
+        answers: [
+          {
+            question: { type: String, required: true },
+            selectedAnswer: { type: String, required: true },
+            correctAnswer: { type: String, required: true },
+          }
+        ],
+        correctAnswers: { type: Number, required: true },
+        timestamp: {
+          type: Date,
+          default: Date.now,
+        },
       }
-    }
-  ],
-  timer: { type: Number },
-  createdAt: { type: Date, default: Date.now }
-});
+    ],
+    timestamp: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
 
 const FaceOffAnswer = mongoose.model('FaceOffAnswer', faceOffAnswerSchema);
 module.exports = FaceOffAnswer;
